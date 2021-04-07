ceb20a3dd827d3f8dc6288fad0f4491f1cd3fd83
Muskaan Singh <muskaansingh235@gmail.com>
2021-04-07 10:51:27 +0530

    render data on the UI

--- src/Components/ProductComponent.js
+++ src/Components/ProductComponent.js
@@ -3,22 +3,32 @@ import {useSelector} from 'react-redux';
 
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products);
-    // const {id, title, price} = products[0]
-    return (
-        <div className="four column wide">
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image">
-                        <img src="https://image.shutterstock.com/image-vector/new-collection-summer-handwritten-text-260nw-372574357.jpg"/>
-                    </div>
-                    <div className="content">
-                        {/* <div className="header">{title}</div>
-                        <div className="header">{price}</div> */}
+
+    const RenderList = products.map((product) => {
+        const {id, title, price, category,image} = product
+        return(
+            <div className="four wide column" key={id}>
+                <div className="ui link cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image} alt={title}/>
+                        </div>
+                        <div className="content">
+                            <div className="header">{title}</div>
+                            <div className="meta price">$ {price}</div>
+                            <div className="meta">{category}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    });
+
+    return(
+        <>
+            {RenderList}
+        </>  
+    );
 }
 
 export default ProductComponent
